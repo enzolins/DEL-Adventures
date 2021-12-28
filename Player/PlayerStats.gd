77dbc29877b05636e4709ccb4d5ae33d31615cf0
enzolins <85700008+enzolins@users.noreply.github.com>
2021-12-28 17:56:12 -0300

    Player Stats and Health UI

---
+++ Player/PlayerStats.gd
@@ -0,0 +1,37 @@
+extends Node
+
+export(int) var max_health = 1 setget setMaxHealth
+var health = max_health setget setHealth
+export(int) var limitHealth = 7
+
+#export(int) var gold = 0 setget setGold
+
+signal noHealth
+signal healthChanged(value)
+signal maxHealthChanged(value)
+signal goldChanged(value)
+
+func setHealth(value):
+	health = value
+	emit_signal("healthChanged", health)
+	if health <= 0:
+		emit_signal("noHealth")
+
+func setMaxHealth(value):
+	if max_health < limitHealth:
+		max_health = value
+		self.health = min(health, max_health)
+		emit_signal("maxHealthChanged",max_health)
+	else:
+		pass
+		
+#func setGold(value):
+#	gold += value
+#	emit_signal("goldChanged",gold)
+	
+#func restartGold(value):
+#	gold = value
+#	emit_signal("goldChanged",gold)
+	
+func _ready():
+	self.health = max_health
---
+++ UI/Hearts/HealthUI.gd
@@ -0,0 +1,24 @@
+extends Control
+
+var hearts = 4 setget setHearts
+var max_hearts = 4 setget setMaxHearts
+
+onready var heartFull = $HeartFull
+onready var heartEmpty = $HeartEmpty
+
+func setHearts(value):
+	hearts = clamp(value, 0, max_hearts)
+	if heartFull != null:
+		heartFull.rect_size.x = hearts * 15
+	
+func setMaxHearts(value):
+	max_hearts = max(value, 1)
+	self.hearts = min(hearts, max_hearts)
+	if heartEmpty != null:
+		heartEmpty.rect_size.x = max_hearts * 15
+	
+func _ready():
+	self.max_hearts = PlayerStats.max_health
+	self.hearts = PlayerStats.health
+	PlayerStats.connect("healthChanged", self, "setHearts")
+	PlayerStats.connect("maxHealthChanged", self, "setMaxHearts")
